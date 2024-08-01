c88c597643ba782f69bc880cd276cdb8c624e303
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-08-01 14:26:50 +0300

    feat: add base styles, fonts, components etc.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,69 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+import Head from 'next/head'
+
+import '@/app/styles/index.scss'
+
+const narrowBlack = localFont({
+  src: [
+    {
+      path: '../public/fonts/Obviously-NarrowBlack.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Obviously-NarrowBlack.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  preload: true,
+  display: 'swap',
+})
+
+const matter = localFont({
+  src: [
+    {
+      path: '../public/fonts/Matter-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Matter-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Matter-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Matter-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  preload: true,
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-family-primary: ${narrowBlack.style.fontFamily};
+          --font-family-secondary: ${matter.style.fontFamily};
+        }
+      `}</style>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
