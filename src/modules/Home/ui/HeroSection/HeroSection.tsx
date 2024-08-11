7375c2da8969a9e8ca872704babd03e409da3f12
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-08-11 15:23:52 +0300

    fix: hero design fixes

--- src/modules/Home/ui/HeroSection/HeroSection.tsx
+++ src/modules/Home/ui/HeroSection/HeroSection.tsx
@@ -4,24 +4,38 @@ import classNames from 'classnames'
 import PrimaryButton from '@/ui/PrimaryButon'
 
 import styles from './HeroSection.module.scss'
+import { BackgroundImage } from '@/ui/BackgroundImage/BackgroundImage'
 
 const HeroSection = () => {
   return (
     <section className={styles['hero']}>
       <Container>
         <div className={styles['hero__content']}>
-          <h1 className={classNames('h1', styles['title'])}>
-            <span>HIRE S-TIER SHORT-FORM</span>
-            <br /> CONTENT CREATORS
-            <br /> IN A FEW CLICKS
-          </h1>
-          <p className={styles['description']}>
-            Get matched with top video editors, UGC creators and video ads
-            creators. 1st video is free.
-          </p>
-          <PrimaryButton className={styles['button']} size={'large'}>
-            Hire top video creators
-          </PrimaryButton>
+          <div className={styles['hero__content_left']}>
+            <h1 className={classNames('h1', styles['title'])}>
+              HIRE S-TIER
+              <br /> <span>SHORT-FORM</span>
+              <br /> <span>CONTENT CREATORS</span>
+              <br /> IN A FEW CLICKS
+            </h1>
+          </div>
+          <div className={styles['hero__content_right']}>
+            <BackgroundImage
+              src={'/images/hero/hero_people.png'}
+              alt={'hero-image'}
+              className={styles['image']}
+            />
+            <p>
+              Get matched with top video editors, UGC creators and video ads
+              creators.
+            </p>
+            <PrimaryButton className={styles['button']} size={'large'}>
+              Hire top video creators
+            </PrimaryButton>
+            <p>
+              <span>1st video is free</span>
+            </p>
+          </div>
         </div>
       </Container>
     </section>
